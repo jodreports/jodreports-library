ff705459bdc4e1210612cf448182937b1dd4af00
jacob.fahrenkrug <jacob.fahrenkrug@hypoport.de>
2012-01-25 21:21:47 +0100

    restored tests

---
+++ src/test/java/org/jodreports/opendocument/OpenDocumentArchiveTest.java
@@ -0,0 +1,45 @@
+package org.jodreports.opendocument;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import org.jodreports.opendocument.OpenDocumentArchive;
+
+import org.apache.commons.io.IOUtils;
+
+public class OpenDocumentArchiveTest extends TestCase {
+
+	public void testCopy() throws IOException {
+		OpenDocumentArchive archive = new OpenDocumentArchive();
+
+		Writer mimetypeWriter = archive.getEntryWriter(OpenDocumentArchive.ENTRY_MIMETYPE);
+		mimetypeWriter.write("application/x-test");
+		mimetypeWriter.close();
+
+		Writer contentWriter = archive.getEntryWriter(OpenDocumentArchive.ENTRY_CONTENT);
+		contentWriter.write("test content");
+		contentWriter.close();
+
+		OpenDocumentArchive archiveCopy = archive.createCopy();
+		assertNotNull(archiveCopy);
+		Set entryNames = archiveCopy.getEntryNames();
+		assertNotNull(entryNames);
+		assertEquals(2, entryNames.size());
+		assertTrue(entryNames.contains(OpenDocumentArchive.ENTRY_MIMETYPE));
+		assertTrue(entryNames.contains(OpenDocumentArchive.ENTRY_CONTENT));
+
+		assertEquals("test content", IOUtils.toString(archiveCopy.getEntryReader(OpenDocumentArchive.ENTRY_CONTENT)));
+
+		// now modify the content
+		Writer contentCopyWriter = archiveCopy.getEntryWriter(OpenDocumentArchive.ENTRY_CONTENT);
+		contentCopyWriter.write("modified content");
+		contentCopyWriter.close();
+
+		assertEquals("test content", IOUtils.toString(archive.getEntryReader(OpenDocumentArchive.ENTRY_CONTENT)));
+		assertEquals("modified content", IOUtils.toString(archiveCopy.getEntryReader(OpenDocumentArchive.ENTRY_CONTENT)));
+	}
+}
+
